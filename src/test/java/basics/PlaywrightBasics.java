cbb3203c503b165ae900b23c07406a3743c5dac9
DecentSuhas <suhasnair.cta@gmail.com>
2025-03-05 09:36:20 +0530

    First file

---
+++ src/test/java/basics/PlaywrightBasics.java
@@ -0,0 +1,35 @@
+package basics;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class PlaywrightBasics {
+
+    public static void main(String [] args){
+
+        Playwright playwright = Playwright.create();
+
+        Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        Page page = browser.newPage();
+
+        page.navigate("https://www.amazon.in/");
+
+        String title = page.title();
+
+        System.out.println("Page title is: "+title);
+
+        String url = page.url();
+
+        System.out.println("URL is: "+url);
+
+        browser.close();
+
+        playwright.close();
+
+
+
+    }
+}
